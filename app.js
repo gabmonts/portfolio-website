d0c1e65a2638600877e274d0b161902b947a6986
Gabriel de Monts <gabriel.demonts@gmail.com>
2023-01-03 11:57:39 +0100

    completed home-page

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const sections = document.querySelectorAll('.section')
 const sectBtns = document.querySelectorAll('.controlls')
 const sectBtn = document.querySelectorAll('.control')
-const allsections = document.querySelectorAll('.main-content')
+const allSections = document.querySelectorAll('.main-content')
 
 function pageTransitions(){
   for(let i = 0; i < sectBtn.length; i++){
     sectBtn[i].addEventListener('click', function(){
+
+      // Removing "active" on old button
       let currentBtn = document.querySelector('.active-btn');
       currentBtn.classList.remove('active-btn')
+
+      // Adding "active" on new button
       this.classList.add('active-btn');
+
+      // Removing "active" on old section
+      let currentSect = document.querySelector('.active-sect')
+      currentSect.classList.remove('active-sect')
+
+      // Adding "active" on new section
+      let id = this.dataset.id
+      let newSection = document.getElementById(id)
+      newSection.classList.add('active-sect')
+
     })
   }
 }
